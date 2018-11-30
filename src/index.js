8685d47ff43923de57c1b8532a3081d3f573a307
Michal Krautforst <michal.krautforst@ioof.com.au>
2018-11-30 11:34:11 +1100

    created react app with amozon cognito login

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from "aws-amplify-react";
+import aws_config from "./aws-exports";
+
+Amplify.configure(aws_config);
+const AppWithAuth = withAuthenticator(App)
+
+ReactDOM.render(<AppWithAuth />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
